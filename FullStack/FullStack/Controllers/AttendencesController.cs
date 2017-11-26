095f13ac950c8b7b869bd8f07ddafbf01096cf20
rushd <rushd@LAPTOP-091TSHH6>
2017-11-26 20:45:12 +0000

    add Attendence model, create api call

---
+++ FullStack/FullStack/Controllers/AttendencesController.cs
@@ -0,0 +1,38 @@
+﻿using FullStack.Dto;
+using FullStack.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace FullStack.Controllers
+{
+    [Authorize]
+    public class AttendencesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendencesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userid = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userid && a.GigId == dto.GigId))
+                return BadRequest("The attendace already exists ");
+
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userid
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ FullStack/FullStack/Dto/AttendanceDto.cs
@@ -0,0 +1,7 @@
+namespace FullStack.Dto
+{
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+}
